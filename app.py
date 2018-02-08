33155338b684cd4000e9fc72726fc74367f62b64
T. Brian <twijukyevascubrian@gmail.com>
2018-02-08 12:53:23 +0300

    Project Files

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('WelcomeAfterLogin.html')
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+    #session you user logged in ...............
+        session['logged_in'] = True
+    #setting sessions............................
+        session['user_name']=request.form['username']
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+@app.route("/add_user")
+def add_user():
+    session['switch_panel']=request.args.get('my_var', '')  
+    return render_template('WelcomeAfterLogin.html', my_var=request.args.get('my_var', ''))
+ 
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
